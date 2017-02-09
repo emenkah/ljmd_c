30fb5a7303d4c7428ca1940416376284d9c45a44
asartori86 <alberto.sartori86@gmail.com>
2017-02-09 11:58:58 +0100

    added static

--- include/constants.h
+++ include/constants.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 /* a few physical constants */
-const double kboltz=0.0019872067;     /* boltzman constant in kcal/mol/K */
-const double mvsq2e=2390.05736153349; /* m*v^2 in kcal/mol */
+static const double kboltz=0.0019872067;     /* boltzman constant in kcal/mol/K */
+static const double mvsq2e=2390.05736153349; /* m*v^2 in kcal/mol */
 
 #endif
