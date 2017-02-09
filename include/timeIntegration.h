d87fbe66a95c712a9744154bbc33030cdb3219ec
Simone Martini <simomartini@MacBook-di-Simone-2.local>
2017-02-09 16:18:47 +0100

    time integration done

---
+++ include/timeIntegration.h
@@ -0,0 +1,11 @@
+#ifndef TIMEINTEGRATION_H
+#define TIMEINTEGRATION_H
+
+#include <data.h>
+#include <constants.h>
+#include <utilities.h>
+#include <force_kinerg.h>
+
+void velverlet(mdsys_t *sys);
+
+#endif
--- src/.#ljmd.c
+++ src/.#ljmd.c
@@ -1,1 +1,1 @@
-simomartini@nb-20-148.ictp.it.7637
+simomartini@nb-20-148.ictp.it.7819
